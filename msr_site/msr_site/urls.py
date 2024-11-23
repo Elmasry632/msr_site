82663cdd284bf5d1c2d730b94e0481206438b887
Elmasry632 <ibrahimelmasry631@gmail.com>
2024-11-23 16:42:56 +0200

    {modified_app}

--- msr_site/msr_site/urls.py
+++ msr_site/msr_site/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.http import HttpResponse
 def about(request):
     return HttpResponse("This is about page")
@@ -28,4 +28,5 @@ urlpatterns = [
     path('about', about),
     path('masry', masry),
     path('contact', contact),
+    path('blog/',include('blog.urls')),
 ]
